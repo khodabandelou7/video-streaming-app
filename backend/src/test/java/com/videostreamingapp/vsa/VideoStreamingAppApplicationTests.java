839254c9af1ca8b6a15ae8ebcdc64b532f9b0a63
khodabandelou7 <r.khodabandelou@gmail.com>
2022-08-09 14:54:59 +0430

    Initial commit.

---
+++ backend/src/test/java/com/videostreamingapp/vsa/VideoStreamingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.videostreamingapp.vsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoStreamingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
